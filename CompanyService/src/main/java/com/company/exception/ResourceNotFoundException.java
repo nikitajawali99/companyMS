bbadce85bca0af5614e4c6cb8fc564f2305d0171
Shaneen <jawalinikita06@gmail.com>
2023-11-07 21:54:46 +0530

    company code updated

--- CompanyService/src/main/java/com/company/exception/ResourceNotFoundException.java
+++ CompanyService/src/main/java/com/company/exception/ResourceNotFoundException.java
@@ -2,6 +2,8 @@ package com.company.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
 
+	private static final long serialVersionUID = 1L;
+
 	public ResourceNotFoundException() {
 		super("Reasource not found");
 	}
